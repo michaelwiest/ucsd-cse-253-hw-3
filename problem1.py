20a6f88b42158d4229950571e8be2ad742bd6e6b
Michael Wiest <michaelwiest@Michaels-MacBook-Pro-3.local>
2018-02-07 15:59:21 -0800

    more printing.

--- problem1.py
+++ problem1.py
@@ -80,6 +80,7 @@ for epoch in range(2):  # loop over the dataset multiple times
 
         # print statistics
         running_loss += loss.data[0]
+        print('Completed minibatch')
         if i % 2000 == 1999:    # print every 2000 mini-batches
             print('[%d, %5d] loss: %.3f' %
                   (epoch + 1, i + 1, running_loss / 2000))
