89827da26917954ebf7e0796f68d9ea6f09c6705
Michael Wiest <michaelwiest@Michaels-MacBook-Pro-3.local>
2018-02-07 15:42:57 -0800

    no download

--- problem1.py
+++ problem1.py
@@ -34,12 +34,12 @@ transform = transforms.Compose(
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
 trainset = torchvision.datasets.CIFAR10(root='./datasets/CIFAR-10', train=True,
-                                        download=True, transform=transform)
+                                        transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
                                           shuffle=True, num_workers=2)
 
 testset = torchvision.datasets.CIFAR10(root='./datasets/CIFAR-10', train=False,
-                                       download=True, transform=transform)
+                                       transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=4,
                                          shuffle=False, num_workers=2)
 
