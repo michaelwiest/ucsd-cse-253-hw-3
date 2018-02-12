2761648cdc410f548d4b9791a8e6b62cbdeda453
Michael Wiest <michaelwiest@Michaels-MacBook-Pro-3.local>
2018-02-12 14:23:04 -0800

    input size change.

--- problem1.py
+++ problem1.py
@@ -50,8 +50,8 @@ class Net(nn.Module):
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.conv2 = nn.Conv2d(6, 10, 5)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv3 = nn.Conv2d(10, 10, 5)
-        self.conv4 = nn.Conv2d(10, 16, 5)
+        self.conv3 = nn.Conv2d(10, 12, 5)
+        self.conv4 = nn.Conv2d(12, 16, 5)
 
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
         torch.nn.init.xavier_uniform(self.fc1.weight)
