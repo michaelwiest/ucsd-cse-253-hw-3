517572575a1e46906f025684f400d56f0b2f70ff
Michael Wiest <michaelwiest@Michaels-MacBook-Pro-3.local>
2018-02-12 13:14:15 -0800

    network reordering.

--- problem1.py
+++ problem1.py
@@ -52,7 +52,7 @@ class Net(nn.Module):
         # self.conv2 = nn.Conv2d(6, 6, 5)
         # self.conv3 = nn.Conv2d(6, 1, 10)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv3 = nn.Conv2d(6, 16, 5)
+        # self.conv3 = nn.Conv2d(6, 16, 5)
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
